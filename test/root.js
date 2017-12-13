2833115314351465db5d11796ead5c880c56a70b
ruuuh20 <pkim2320@gmail.com>
2017-12-12 22:56:15 -0500

    Done.

--- test/root.js
+++ test/root.js
@@ -15,3 +15,21 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
+
+
+//added this enzyme part because of error
+
+import { configure } from 'enzyme';
+ import Adapter from 'enzyme-adapter-react-16';
+ configure({ adapter: new Adapter() });
+
+
+ // Error:
+ //      Enzyme Internal Error: Enzyme expects an adapter to be configured, but found none. To
+ //      configure an adapter, you should call `Enzyme.configure({ adapter: new Adapter() })`
+ //      before using any of Enzyme's top level APIs, where `Adapter` is the adapter
+ //      corresponding to the library currently being tested. For example:
+ //
+ //      import Adapter from 'enzyme-adapter-react-15';
+ //
+ //      To find out more about this, see http://airbnb.io/enzyme/docs/installation/index.html
